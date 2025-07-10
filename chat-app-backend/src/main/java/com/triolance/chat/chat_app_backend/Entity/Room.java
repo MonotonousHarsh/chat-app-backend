149ff8f76afe72262b031ac6a6fe5c99bcaf139a
Harsh Saxena <saxenaharsh50984@gmail.com>
2025-07-10 23:54:56 +0530

    Initial commit

---
+++ chat-app-backend/src/main/java/com/triolance/chat/chat_app_backend/Entity/Room.java
@@ -0,0 +1,12 @@
+package com.triolance.chat.chat_app_backend.Entity;
+
+
+import java.lang.annotation.Documented;
+
+@D
+public class Room {
+
+
+
+
+}
---
+++ chat-app-backend/src/main/java/com/triolance/chat/chat_app_backend/Entity/User.java
@@ -0,0 +1,7 @@
+package com.triolance.chat.chat_app_backend.Entity;
+
+@
+public class User {
+
+    public
+}
