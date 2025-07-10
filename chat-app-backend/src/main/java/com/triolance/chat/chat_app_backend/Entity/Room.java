0d3084d676402ba46f21919a5fd8582a862bfb6a
Harsh Saxena <saxenaharsh50984@gmail.com>
2025-07-11 00:20:07 +0530

    begin

--- chat-app-backend/src/main/java/com/triolance/chat/chat_app_backend/Entity/Room.java
+++ chat-app-backend/src/main/java/com/triolance/chat/chat_app_backend/Entity/Room.java
@@ -1,10 +1,53 @@
 package com.triolance.chat.chat_app_backend.Entity;
 
 
-import java.lang.annotation.Documented;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.logging.log4j.message.Message;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
-@D
+import java.lang.annotation.Documented;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "Room_collection")
 public class Room {
+
+    @Id
+    private ObjectId id;
+
+    @Field("roomId")
+    private String roomId;
+
+    @Field("messages")
+    private List<Message>messages = new ArrayList<>();
+
+    public static class Message{
+
+        @Field("username")
+        private String username;
+
+        @Field("content")
+        private String content;
+
+        private Instant timestamp;
+
+
+    }
+
+        public static void addMessage(Message message){
+
+        }
 
 
 
--- chat-app-backend/src/main/java/com/triolance/chat/chat_app_backend/Entity/User.java
+++ chat-app-backend/src/main/java/com/triolance/chat/chat_app_backend/Entity/User.java
@@ -1,6 +1,30 @@
 package com.triolance.chat.chat_app_backend.Entity;
 
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Document(collection = "users")
 public class User {
-    
+
+    @Id
+    private Object id;
+
+    @NonNull
+    private String username;
+
+    @NonNull
+    private  String password;
+
+    @NonNull
+    private String email;
+
 }
